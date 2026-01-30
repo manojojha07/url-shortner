ce65accef68feb330dfe91ed3009ef4a4a1b68fb
manoj-ojha04 <manojojha154@gmail.com>
2026-01-30 21:34:46 +0530

    change frontend URL

--- frontend/src/Components/UseUrl.jsx
+++ frontend/src/Components/UseUrl.jsx
@@ -85,12 +85,12 @@ const UserUrl = () => {
                 <td className="px-6 py-4">
                   <div className="text-sm">
                     <a 
-                      href={`http://localhost:3000/${url.short_url}`} 
+                      href={`https://url-shortner-jt7o.onrender.com${url.short_url}`} 
                       target="_blank" 
                       rel="noopener noreferrer"
                       className="text-blue-600 hover:text-blue-900 hover:underline"
                     >
-                      {`localhost:3000/${url.short_url}`}
+                      {`https://url-shortner-jt7o.onrender.com${url.short_url}`}
                     </a>
                   </div>
                 </td>
